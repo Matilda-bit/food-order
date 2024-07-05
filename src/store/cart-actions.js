ae4961e897733e0f5eb070390594b87889796ca5
Matilda-bit <polinaovras@gmail.com>
2024-07-05 20:14:14 +0300

    fixed fetch logic

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -19,7 +19,10 @@ export const fetchCartData = () => {
 
           try {
              const cartData = await fetchData();
-             dispatch(cartActions.replaceCart(cartData));
+             dispatch(cartActions.replaceCart({
+                items: cartData.items || [],
+                totalQuantity:cartData.totalQuantity,
+             }));
           } catch (error) {
             dispatch(
                 uiActions.showNotification({
@@ -46,7 +49,10 @@ export const sendCartData = (cart) => {
                 'https://food-order-2024-default-rtdb.firebaseio.com/cart.json', 
                 {
                 method: 'PUT',
-                body: JSON.stringify(cart),
+                body: JSON.stringify({
+                    items: cart.items,
+                    totalQuantity: cart.totalQuantity,
+                }),
               });
         
               if(!response.ok){
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -6,6 +6,7 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
     replaceCart(state, action) {
@@ -13,9 +14,10 @@ const cartSlice = createSlice({
       state.items = action.payload.items;
     },
     addItemToCart(state, action) {
-      const newItem = action.payload;
-      const existingItem = state.items.find((item) => item.id === newItem.id);
-      state.totalQuantity++;
+        const newItem = action.payload;
+        const existingItem = state.items.find((item) => item.id === newItem.id);
+        state.totalQuantity++;
+        state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -33,10 +35,12 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
